90febcee6ad156f9d5fd6d4737dbef25e319faee
bhavaniss <bhavanisoftwaresolutions@gmail.com>
2017-08-03 23:39:38 +0530

    minor

--- others/reverseOfANum.c
+++ others/reverseOfANum.c
@@ -4,10 +4,13 @@ int findReverse(int x) {
 	int tempMain = 0;
 	int temp;
 	while(x != 0) {
+		//pick the last digit.
 		temp = x%10;
+		// append the next digit on the right side.
 		tempMain = (tempMain * 10) + temp;
 		// printing number here for understanding.
 		// printf("%d\n", tempMain);
+		//remove the last digit
 		x = x/10;
 	}
 	return tempMain;
